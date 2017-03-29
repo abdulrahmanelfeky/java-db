203484920a66c18dea705b5a44bd6d39c9d46839
abdulrahman elfeky <abdulrahmanelfeky@abdulrahman-elfeky.local>
2017-03-29 05:27:09 +0200

    insert into database

---
+++ build/generated-sources/ap-source-output/javaapplication9/Student_.java
@@ -0,0 +1,17 @@
+package javaapplication9;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-29T05:13:55")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, String> fname;
+    public static volatile SingularAttribute<Student, String> lname;
+    public static volatile SingularAttribute<Student, String> phone;
+    public static volatile SingularAttribute<Student, Integer> id;
+    public static volatile SingularAttribute<Student, String> email;
+
+}
